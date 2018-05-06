acb9a74bf37d5a2ebfb690a9fa88f9c101f4b337
abelevychDat <abelevych@yahoo.com>
2018-05-06 10:06:42 -0400

    final version

--- Run_Analysis.R
+++ Run_Analysis.R
@@ -1,6 +1,5 @@
 ## This script merges the training and the test sets of Samsung data:
 ## (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
-## It extracts only  mean and standard deviation for each measurement. 
 ## It creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 # read train datasets
